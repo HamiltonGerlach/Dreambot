96634615098c27c293e649cc9d84917ad07be9f6
Hamilton Gerlach <aboehmvie@gmail.com>
2024-01-30 18:40:21 +0100

    Added Ableton Control scripts and deploy script

---
+++ Ableton/Dreambot/__init__.py
@@ -0,0 +1,11 @@
+
+import Live
+from .Dreambot import Dreambot
+
+def create_instance(c_instance):
+    ' Creates and returns the APC20 script '
+    return Dreambot(c_instance)
+
+
+# local variables:
+# tab-width: 4
---
+++ Ableton/deploy.cmd
@@ -0,0 +1,2 @@
+robocopy ".\Dreambot" "%PROGRAMDATA%\Ableton\Live 11 Suite\Resources\MIDI Remote Scripts\Dreambot" /s
+timeout /t 3
